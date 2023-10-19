bceb9a870616089cdc7b0c3aa3279f94e696faba
PedroYep <psyepez2@utpl.edu.ec>
2023-10-19 17:24:48 -0500

    del profe

--- psyepez2-MiPrimerPrograma/src/psyepez2/miprimerprograma/Psyepez2MiPrimerPrograma.java
+++ psyepez2-MiPrimerPrograma/src/psyepez2/miprimerprograma/Psyepez2MiPrimerPrograma.java
@@ -1,7 +1,7 @@
 package psyepez2.miprimerprograma;
 public class Psyepez2MiPrimerPrograma {
     public static void main(String[] args) {
-        System.err.println("Hola Mundo");
+        System.err.println("Hola Mundo -profe");
     }
     
 }
