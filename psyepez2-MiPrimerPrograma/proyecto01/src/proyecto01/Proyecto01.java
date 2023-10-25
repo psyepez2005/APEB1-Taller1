b8d0857ed8230a022d7fbd77a10a101b95aa31ea
PedroYep <psyepez2@utpl.edu.ec>
2023-10-24 23:07:10 -0500

    primer commit

---
+++ psyepez2-MiPrimerPrograma/proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,21 @@
+Ejemplo básico de Java
+Versionado a través de Git
+Manejado desde GitHub
+Recuerde usar la sentencia
+
+package proyecto01;
+
+/**
+ *
+ * @author yeped
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
