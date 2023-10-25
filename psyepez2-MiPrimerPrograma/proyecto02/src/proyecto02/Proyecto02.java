1c93daaed1cdd8f29dd0bee3ff54d579c3b8e7c4
PedroYep <psyepez2@utpl.edu.ec>
2023-10-24 23:52:22 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ psyepez2-MiPrimerPrograma/proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,20 @@
+Ejemplo dos básico de Java
+Versionado a través de Git
+Manejado desde GitHub
+Además se usar GitHubClassroom
+package proyecto02;
+
+/**
+ *
+ * @author yeped
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico 2 de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
